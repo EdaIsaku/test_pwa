3979f24046c4ca56529204c388b7163ad5e9c14e
Eda <eda.isaku@yahoo.com>
2023-08-22 22:09:37 +0200

    ADD: Registered service worker

---
+++ public/serviceWorker.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", (event) => {
+  console.log(`[Service Worker] Installing service worker, ${event}`);
+});
+
+self.addEventListener("activate", (event) => {
+  console.log(`[Service Worker] Activating service worker, ${event}`);
+  return self.clients.claim();
+});
